38af628c55d9ddc7f44e34bc8dc1e4e97f769de1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-25 18:55:03 -0300

    feat(home): add about section to the Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
-import { HomeHeroSection } from "./components/HomeHeroSection";
+import { contentfulService } from "@/infra/contentful/ContentfulService";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const data = await contentfulService.getHomePage();
+  
+  console.log(data)
   return (
-    <div className="">
-      <HomeHeroSection 
-        title="Ana Maria Leite"
-        subtitle="Designer de interiores"
-        description="Olá! Sou designer de interiores e transformo ambientes em espaços únicos, funcionais e cheios de estilo."
-      />
-    </div>
+    <main className="">
+      <div className="container py-[1.875rem] px-4 border-dashed border-outline border-b border-r border-l">
+        <div className="flex items-center gap-4 mb-6">
+          <Image 
+            src={data.profilePicture.url}
+            alt="Profile Picture"
+            width={80}
+            height={80}
+            priority
+            className="rounded-full"
+          />
+          <div>
+            <h1 className="text-md font-semibold">{data.name}</h1>
+            <strong className="text-xs text-card-foreground font-normal">{data.jobTitle}</strong>
+          </div>
+        </div>
+        <section className="flex flex-col gap-8 py-[32px]">
+          <h2 className="text-xs uppercase font-normal text-accent-foreground">About</h2>
+          <p className="text-3xl">
+            <span>{data.headline}</span>
+            <br />
+            <span className="text-card-foreground leading-[42px]">{data.headline2}</span>
+          </p>
+          <p className="paragraph">{data.about}</p>
+        </section>
+      </div>
+
+    </main>
   );
 }
