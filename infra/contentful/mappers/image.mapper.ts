f4eb28961f83c6d950ee4c3036b3a27c0c6d6814
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-25 18:16:51 -0300

    feat(cms): add imageMapper function for mapping ImageEntry to ImageCMS

---
+++ infra/contentful/mappers/image.mapper.ts
@@ -0,0 +1,17 @@
+import { ImageCMS } from "@/domain/cms/CMSTypes";
+import { ImageEntry } from "@/domain/contentful/ImageType";
+
+export function imageMapper(entry: ImageEntry): ImageCMS {
+  return {
+    contentType: entry.fields.contentType || '',
+    filename: entry.fields.file.fileName || '',
+    url: entry.fields.file.url || '',
+    details: {
+      image: {
+        height: entry.fields.file.details.height || 0,
+        width: entry.fields.file.details.width || 0,
+      },
+      size: entry.fields.file.details.size || 0,
+    }
+  }
+}
