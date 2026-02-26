3dc7369ba8ed818f7661c12ad8ac836a6fdbbb98
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 15:21:40 -0300

    feat(seoContentfulMapper): rename function for clarity and consistency

--- infra/contentful/mappers/seoContentful.mapper.ts
+++ infra/contentful/mappers/seoContentful.mapper.ts
@@ -1,7 +1,7 @@
 import { SeoCMS } from "@/domain/cms/SeoCMS";
 import { SeoContentful } from "@/domain/contentful/SeoContentful";
 
-export function seoContentful(entry: SeoContentful): SeoCMS {
+export function seoContentfulMapper(entry: SeoContentful): SeoCMS {
   return {
     title: entry?.fields?.title || '',
     description: entry?.fields?.description || '',
