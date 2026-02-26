db0b86e62c80cd3ce5382163ad9b0ddf7f859e23
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 15:21:54 -0300

    feat(CMSService): add getProjectBySlug method to retrieve project by slug

--- domain/cms/CMSService.ts
+++ domain/cms/CMSService.ts
@@ -4,5 +4,6 @@ import { ProductPageCMS } from "./ProductPageCMS";
 export interface CMSService {
   getHomePage(): Promise<HomePageCMS>;
   getProjectsFeatured (): Promise<ProductPageCMS[]>
+  getProjectBySlug (slug: string): Promise<ProductPageCMS | null>
 }
 
