f97492ea8c74a9a67cad09eb6f90b6842aa05f27
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 13:41:25 -0300

    feat(ContentfulService): implement caching for getHomePage and getProjectsFeatured methods

--- infra/contentful/ContentfulService.ts
+++ infra/contentful/ContentfulService.ts
@@ -6,30 +6,38 @@ import { type HomePageCMS } from '@/domain/cms/HomePageCMS';
 import { homePageMapper } from './mappers/homePage.mapper';
 import { productEntriesMapper } from './mappers/productEntries.mapper';
 import { ProductPageCMS } from '@/domain/cms/ProductPageCMS';
+import { cache } from 'react';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID!,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!
 });
 
+const getProjectsFeaturedCached = cache(async (contentfulClient: ContentfulClientApi<undefined>) => {
+  const response = await contentfulClient.getEntries({
+    content_type: 'project',
+    'fields.isFeatured': true,
+  });
+
+  return productEntriesMapper(response as any)
+})
+
+const getHomePageCached = cache(async (contentfulClient: ContentfulClientApi<undefined>) => {
+  const response = await contentfulClient.getEntry('44xE0wVPVZdNBfqLc5nQoC');
+
+  return homePageMapper(response as any);
+})
 export class ContentfulService implements CMSService {
   constructor(
     private readonly contentfulClient: ContentfulClientApi<undefined> = client
   ) {}
 
   async getHomePage(): Promise<HomePageCMS> {
-    const response = await this.contentfulClient.getEntry('44xE0wVPVZdNBfqLc5nQoC');
-
-    return homePageMapper(response as any); 
+    return getHomePageCached(this.contentfulClient);
   }
 
   async getProjectsFeatured (): Promise<ProductPageCMS[]> {
-    const response = await this.contentfulClient.getEntries({
-      content_type: 'project',
-      'fields.isFeatured': true,
-    });
-
-    return productEntriesMapper(response as any);
+    return getProjectsFeaturedCached(this.contentfulClient);
   }
 }
 
