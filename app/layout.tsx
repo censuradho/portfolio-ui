378194c6291691759824e73005149c85964c91f9
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 19:39:36 -0300

    fix(layout): remove lang attribute from html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html>
       <body
         className={`${figtree.variable} antialiased`}
       >
