fdba021fa663c0d01b89c1c0a1c31c0cde7206f5
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 13:41:16 -0300

    feat(homePageMapper): add seo mapping to homePageMapper function

--- infra/contentful/mappers/homePage.mapper.ts
+++ infra/contentful/mappers/homePage.mapper.ts
@@ -2,14 +2,17 @@ import { HomePageCMS } from "@/domain/cms/HomePageCMS";
 import { HomePage } from "@/domain/contentful/HomePage";
 import { contactSectionMapper } from "./contactSection.mapper";
 import { imageMapper } from "./image.mapper";
+import { seoContentful } from "./seoContentful.mapper";
 
 export function homePageMapper (entry: HomePage): HomePageCMS {
+  console.log(entry)
   return {
     about: entry?.fields?.about || '',
     headline: entry?.fields?.headline || '',
     headline2: entry?.fields?.headline2 || '',
     jobTitle: entry?.fields?.jobTitle || '',
     location: entry?.fields?.location || '',
+    seo: seoContentful(entry?.fields?.seo),
     name: entry?.fields?.name || '',
     techStack: entry?.fields?.techStack || [],
     profilePicture: imageMapper(entry?.fields?.profilePicture),
