de3f636f398423b5229dcdd0943626ccce223fb9
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 15:21:32 -0300

    feat(productEntriesMapper): add content field and seo mapping to product entries

--- infra/contentful/mappers/productEntries.mapper.ts
+++ infra/contentful/mappers/productEntries.mapper.ts
@@ -1,6 +1,7 @@
 import { ProductPageCMS } from "@/domain/cms/ProductPageCMS";
 import { ProductPageEntries } from "@/domain/contentful/ProjectPage";
 import { imageMapper } from "./image.mapper";
+import { seoContentfulMapper } from "./seoContentful.mapper";
 
 export function productEntriesMapper (entries: ProductPageEntries): ProductPageCMS[] {
   return entries.items.map(entry => ({
@@ -11,10 +12,12 @@ export function productEntriesMapper (entries: ProductPageEntries): ProductPageC
     industry: entry.fields.industry,
     isFeatured: entry.fields.isFeatured || false,
     location: entry.fields.location,
+    content: entry.fields.content,
     previewDescription: entry.fields.previewDescription,
     title: entry.fields.title,
     previewImage: imageMapper(entry.fields.previewImage),
     image: imageMapper(entry.fields.image),
     featureImage: imageMapper(entry.fields.featureImage),
+    seo: seoContentfulMapper(entry.fields.seo)
   }))
 }
