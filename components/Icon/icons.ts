5557d33c2e68f9f9dd2de6c8abaf7e52859ed309
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 12:13:10 -0300

    feat(icons): add ArrowRight and ArrowTopRight SVG icons

--- components/Icon/icons.ts
+++ components/Icon/icons.ts
@@ -7,8 +7,12 @@ import Location from '@/public/icons/location.svg';
 import Star from '@/public/icons/star.svg';
 import Telegram from '@/public/icons/telegram.svg';
 import Linkedin from '@/public/icons/linkedin.svg';
+import ArrowTopRight from '@/public/icons/arrowTopRight.svg';
+import ArrowRight from '@/public/icons/arrowRight.svg';
 
 export const icons = {
+  ArrowTopRight,
+  ArrowRight,
   Menu,
   Cloak,
   Download,
