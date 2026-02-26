97a01df7365c8b2f7e1b49f43e85f2d3e3e25228
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-02-26 12:14:28 -0300

    feat(CMSService): add getProjectsFeatured method to retrieve featured projects

--- domain/cms/CMSService.ts
+++ domain/cms/CMSService.ts
@@ -1,6 +1,8 @@
 import { HomePageCMS } from "./HomePageCMS";
+import { ProductPageCMS } from "./ProductPageCMS";
 
 export interface CMSService {
   getHomePage(): Promise<HomePageCMS>;
+  getProjectsFeatured (): Promise<ProductPageCMS[]>
 }
 
