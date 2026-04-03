e6698cb7c3ba3767f184fea704a0a81b60db7090
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-03 12:14:59 -0300

    feat(project): add CTA link to project details in ProjectPage component

--- app/projetos/[slug]/page.tsx
+++ app/projetos/[slug]/page.tsx
@@ -42,6 +42,18 @@ export default async function ProjectPage ({ params }: ProjectPageMetadataParams
         <div className="grid grid-cols-12 gap-6">
           <h1 className="text-4xl md:text-5xl leading-tight col-span-12 md:col-span-8 font-semibold">{project.title}</h1>
           <p className="col-span-12 md:col-span-8 text-card-foreground typography">{project.description}</p>
+          {project.cta && (
+            <div className="col-span-12">
+              <a 
+                href={project.cta.path}
+                target="_blank"
+                className="font-medium transition-colors hover:underline flex items-center gap-2"
+              >
+                {project.cta.label}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
+              </a>
+            </div>
+          )}
         </div>
         <dl className="flex gap-4 md:gap-30 mt-[48px] flex-wrap">
           <div className="flex flex-col gap-2">
